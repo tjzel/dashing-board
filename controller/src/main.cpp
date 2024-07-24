079d0e22626dd17c730f1321cea1222a2864ff54
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-07-24 09:22:47 +0200

    chore: use RAII

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,34 +1,86 @@
 #include <iostream>
+#include <string>
 #include <boost/asio.hpp>
 
-using namespace boost::asio;
+typedef const std::string OBD2Command;
 
-// Function to flush the serial port buffer
-void flush_serial_buffer(serial_port& serial) {
-    boost::asio::streambuf buf;
-    boost::system::error_code ec;
+class Requests {
+public:
+    static OBD2Command RESET;
+    static OBD2Command ECHO_OFF;
+    static OBD2Command ENGINE_RPM;
+private:
+    Requests() = default;
+};
 
-    // Read until no more data is available
-    while (boost::asio::read(serial, buf.prepare(256), transfer_at_least(1), ec)) {
-        buf.consume(buf.size());
+OBD2Command Requests::RESET = "ATZ";
+OBD2Command Requests::ECHO_OFF = "ATE0";
+OBD2Command Requests::ENGINE_RPM = "010C";
+
+class OBD2Handler {
+public:
+    explicit OBD2Handler(const std::string& serialPortPath) : io(), serial(boost::asio::serial_port(io, serialPortPath)) {
+        serial.set_option(boost::asio::serial_port_base::baud_rate(38400));
+        serial.set_option(boost::asio::serial_port_base::character_size(8));
+        serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+        serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+        serial.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
     }
-}
 
-void write_command(serial_port& serial, const std::string& cmd) {
-    std::string delimiter = "\r";
-    write(serial, buffer(cmd + delimiter));
-}
+public:
+    std::string reset() {
+        // For some reason the command is always echoed back
+        auto response = request(Requests::RESET);
+        auto commandLength = Requests::RESET.size() + requestDelimiter.size();
+        response = response.substr(commandLength, response.size() - commandLength);
+        return response;
+    }
 
-std::string read_response(serial_port& serial, const size_t skipFromStart = 0) {
-    boost::asio::streambuf buf;
-    std::string delimiter = "\r\r>";
-    auto delimiter_length = delimiter.size();
-    auto lenght = read_until(serial, buf, delimiter);
-    std::istream is(&buf);
-    std::string response;
-    std::getline(is, response);
-    return response.substr(skipFromStart, lenght - delimiter_length - skipFromStart);
-}
+    std::string echoOff() {
+        return request(Requests::ECHO_OFF);
+    }
+
+    int getRPM() {
+        const auto response = request(Requests::ENGINE_RPM);
+        int RPM;
+        try {
+            const auto A = std::stoi(response.substr(11, 2), nullptr, 16);
+            const auto B = std::stoi(response.substr(14, 2), nullptr, 16);
+            RPM = ((A * 256) + B) / 4;
+        }
+        catch (...) {
+            RPM = -1;
+        }
+        return RPM;
+    }
+
+private:
+    std::string request(OBD2Command& rawCommand) {
+        std::string command = rawCommand + requestDelimiter;
+        write(serial, boost::asio::buffer(command));
+
+        auto responseLength = read_until(serial, inputBuffer, responseDelimiter);
+        std::istream inputStream(&inputBuffer);
+        std::string response;
+        std::getline(inputStream, response);
+        response = response.substr(0, responseLength - responseDelimiter.size());
+        for (char& i : response) {
+            if (i == '\r') {
+                i = '\n';
+            }
+        }
+        return response;
+    }
+
+    boost::asio::io_service io;
+    boost::asio::serial_port serial;
+    boost::asio::streambuf inputBuffer;
+    static const std::string requestDelimiter;
+    static const std::string responseDelimiter;
+};
+
+const std::string OBD2Handler::requestDelimiter = "\r";
+const std::string OBD2Handler::responseDelimiter = "\r\r>";
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -37,44 +89,11 @@ int main(int argc, char* argv[]) {
     }
     const std::string serialPort = argv[1];
     std::cout << "Using serial port: " << serialPort << std::endl;
-    try {
-        io_service io;
-        serial_port serial(io, serialPort);  // Adjust to your device
-        serial.set_option(serial_port_base::baud_rate(38400));
-        serial.set_option(serial_port_base::character_size(8));
-        serial.set_option(serial_port_base::parity(serial_port_base::parity::none));
-        serial.set_option(serial_port_base::stop_bits(serial_port_base::stop_bits::one));
-        serial.set_option(serial_port_base::flow_control(serial_port_base::flow_control::none));
-
-        // flush_serial_buffer(serial);
-        // Initialize OBD-II communication
-        std::cout << "Sending ATZ" << std::endl;
-        write_command(serial, "ATZ");  // Reset
-        std::cout << "Response: " << read_response(serial, 4) << std::endl;
-        // std::cout << "Sending ATE0" << std::endl;
-        // write_command(serial, "ATE0");  // Echo off
-        // std::cout << "Response: " << read_response(serial) << std::endl;
-
-        // Request engine RPM
-        std::cout << "Sending 010C" << std::endl;
-        write_command(serial, "010C");
-        std::string response = read_response(serial, 5);
-
-        // Process response (assuming form 41 0C XX YY)
-        const std::string substr = response.substr(0, 4);
-        if (response.substr(0, 4) == "41 0C") {
-            int A = std::stoi(response.substr(5, 2), nullptr, 16);
-            int B = std::stoi(response.substr(8, 2), nullptr, 16);
-            int rpm = ((A * 256) + B) / 4;
-            std::cout << "RPM: " << rpm << std::endl;
-        }
-        else {
-            std::cout << "Unexpected response: " << response << std::endl;
-        }
-    }
-    catch (boost::system::system_error& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+    OBD2Handler handler(serialPort);
+    std::cout << "Resetting" << std::endl;
+    std::cout << "Response: " << handler.reset() << std::endl;
+    std::cout << "Requesting RPM" << std::endl;
+    std::cout << "Response: " << handler.getRPM() << std::endl;
 
     return 0;
 }
