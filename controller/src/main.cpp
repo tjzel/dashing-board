3a561488a6fc015fa144f39bf1f7d46d96194ce9
Tomasz Żelawski <tomasz.zelawski@swmansion.com>
2024-07-25 09:24:04 +0200

    feat: add command map

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <format>
 #include <boost/asio.hpp>
 
 typedef const std::string OBD2Command;
@@ -9,6 +11,7 @@ public:
     static OBD2Command RESET;
     static OBD2Command ECHO_OFF;
     static OBD2Command ENGINE_RPM;
+    static OBD2Command AVAILABLE_DATA_COMMANDS;
 private:
     Requests() = default;
 };
@@ -16,6 +19,7 @@ private:
 OBD2Command Requests::RESET = "ATZ";
 OBD2Command Requests::ECHO_OFF = "ATE0";
 OBD2Command Requests::ENGINE_RPM = "010C";
+OBD2Command Requests::AVAILABLE_DATA_COMMANDS = "0100";
 
 class OBD2Handler {
 public:
@@ -25,11 +29,14 @@ public:
         serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
         serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
         serial.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+
+        reset();
+        loadAvailableDataCommands();
     }
 
 public:
     std::string reset() {
-        // For some reason the command is always echoed back
+        // For some reason this command is always echoed back.
         auto response = request(Requests::RESET);
         auto commandLength = Requests::RESET.size() + requestDelimiter.size();
         response = response.substr(commandLength, response.size() - commandLength);
@@ -37,25 +44,59 @@ public:
     }
 
     std::string echoOff() {
+        // Disabled by default.
         return request(Requests::ECHO_OFF);
+    }
+
+    int parseRPM(const std::string& response) {
+        int RPM = -1;
+        try {
+            if (response.size() >= 16) {
+                const auto A = std::stoi(response.substr(11, 2), nullptr, 16);
+                const auto B = std::stoi(response.substr(14, 2), nullptr, 16);
+                RPM = ((A * 256) + B) / 4;
+            }
+        }
+        catch (...) { }
+        return RPM;
     }
 
     int getRPM() {
         const auto response = request(Requests::ENGINE_RPM);
-        int RPM;
-        try {
-            const auto A = std::stoi(response.substr(11, 2), nullptr, 16);
-            const auto B = std::stoi(response.substr(14, 2), nullptr, 16);
-            RPM = ((A * 256) + B) / 4;
+        return parseRPM(response);
+    }
+
+    void loadAvailableDataCommands() {
+        const auto response = request(Requests::AVAILABLE_DATA_COMMANDS);
+        const std::string startSequence = "41 00";
+        auto startIndex = response.find(startSequence) + startSequence.size();
+        if (startIndex < 5 || startIndex + 4 * 3 >= response.size()) {
+            return;
         }
-        catch (...) {
-            RPM = -1;
+
+        for (int i = 0; i < 4; i++) {
+            const auto A = std::stoi(response.substr(startIndex + i * 3, 2), nullptr, 16);
+            std::cout << "A: " << A << std::endl;
+            for (int j = 0; j < 8; j++) {
+                const auto key = std::format("{:0{}X}", i * 8 + j, 2);
+                availableCommands[key] = A & (1 << (7 - j));
+            }
+
         }
-        return RPM;
+    }
+
+    void printAvailableCommands() {
+        std::cout << "Available commands:" << std::endl;
+        for (auto& i : availableCommands) {
+            std::cout << i.first << ": " << i.second << std::endl;
+        }
     }
 
 private:
     std::string request(OBD2Command& rawCommand) {
+        if (availableCommands.find(rawCommand) == availableCommands.end()) {
+            return "";
+        }
         std::string command = rawCommand + requestDelimiter;
         write(serial, boost::asio::buffer(command));
 
@@ -69,12 +110,15 @@ private:
                 i = '\n';
             }
         }
+        std::cout << "--- Raw response ---" << std::endl << response << std::endl << "--- End of raw response ---" << std::endl;
         return response;
     }
 
     boost::asio::io_service io;
     boost::asio::serial_port serial;
     boost::asio::streambuf inputBuffer;
+    std::map<std::string, bool> availableCommands{ (std::make_pair(Requests::AVAILABLE_DATA_COMMANDS, true)) };
+
     static const std::string requestDelimiter;
     static const std::string responseDelimiter;
 };
@@ -90,10 +134,9 @@ int main(int argc, char* argv[]) {
     const std::string serialPort = argv[1];
     std::cout << "Using serial port: " << serialPort << std::endl;
     OBD2Handler handler(serialPort);
-    std::cout << "Resetting" << std::endl;
-    std::cout << "Response: " << handler.reset() << std::endl;
     std::cout << "Requesting RPM" << std::endl;
-    std::cout << "Response: " << handler.getRPM() << std::endl;
+    std::cout << "Response: " << std::endl << handler.getRPM() << std::endl;
+    handler.printAvailableCommands();
 
     return 0;
 }
