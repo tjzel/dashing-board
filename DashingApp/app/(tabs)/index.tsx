ddf1e04e923cfab8cfa98382a8da23d1310d0cc1
Tomasz Żelawski <tzelawski@gmail.com>
2025-01-17 11:44:19 +0100

    feat: encapsulate BLE in App

--- DashingApp/app/(tabs)/index.tsx
+++ DashingApp/app/(tabs)/index.tsx
@@ -1,263 +1,31 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import * as ScreenOrientation from "expo-screen-orientation";
 import React from "react";
 import * as NavigationBar from "expo-navigation-bar";
-import { useBLE } from "../../hooks/useBLE";
-import base64 from "react-native-base64";
-import { Characteristic } from "react-native-ble-plx";
-
-interface DataFrame {
-  engineLoad: number;
-  engineRPM: number;
-  vehicleSpeed: number;
-  uptime: number;
-  fuelLevel: number;
-  absoluteLoad: number;
-  relativeThrottlePosition: number;
-  engineFuelRate: number;
-}
-
-interface DataCharacteristics {
-  update: Characteristic;
-  engineLoad: Characteristic;
-  engineRPM: Characteristic;
-  vehicleSpeed: Characteristic;
-  uptime: Characteristic;
-  fuelLevel: Characteristic;
-  absoluteLoad: Characteristic;
-  relativeThrottlePosition: Characteristic;
-  engineFuelRate: Characteristic;
-}
+import { useBleController, type DataFrame } from "../../hooks/useBleController";
 
 export default function HomeScreen() {
-  const [json, setJson] = useState<DataFrame | null>(null);
-  const [chars, setChars] = useState<DataCharacteristics>({});
-  const [isLandscape, setLandscape] = useState(false);
-  NavigationBar.setVisibilityAsync("hidden");
-
-  const {
-    allDevices,
-    connectedDevice,
-    connectToDevice,
-    color,
-    requestPermissions,
-    scanForPeripherals,
-  } = useBLE();
-
-  const scanForDevices = async () => {
-    const isPermissionsEnabled = await requestPermissions();
-    if (isPermissionsEnabled) {
-      scanForPeripherals();
-    }
-  };
+  const [dataFrame, setDataFrame] = useState<DataFrame | null>(null);
+  const [, setLandscape] = useState(false);
+  const bleController = useBleController();
 
   useEffect(() => {
-    allDevices.forEach((device) => {
-      device
-        .connect()
-        .then((connection) => {
-          return connection.discoverAllServicesAndCharacteristics();
-        })
-        .then((discovered) => {
-          // console.log(discovered.serviceData);
-          // console.log(discovered.serviceUUIDs);
-          // return discovered.characteristicsForService(discovered.serviceUUIDs[2]);
-          return discovered.services();
-        })
-        .then((services) => {
-          services.forEach((service) => {
-            console.log(service.uuid);
-            service.characteristics().then((characteristics) => {
-              characteristics.forEach((characteristic) => {
-                console.log(characteristic.uuid);
-                characteristic.readDescriptor("2901").then((read) => {
-                  // console.log(read);
-                  const name = base64.decode(read.value);
-                  // console.log(name);
-                  // console.log(base64.decode(read.value));
-                  const newChars = {};
-                  if (name === "update") {
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   update: characteristic,
-                    // }));
-                    // newChars.update = characteristic;
-                  } else if (name === "engineLoad") {
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   engineLoad: characteristic,
-                    // }));
-                    // newChars.engineLoad = characteristic;
-                  } else if (name === "engineRPM") {
-                    console.log('setting engineRPM');
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   engineRPM: characteristic,
-                    // }));
-                    newChars.engineRPM = characteristic;
-                  } else if (name === "vehicleSpeed") {
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   vehicleSpeed: characteristic,
-                    // }));
-                    // newChars.vehicleSpeed = characteristic;
-                  } else if (name === "uptime") {
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   uptime: characteristic,
-                    // }));
-                    // newChars.uptime = characteristic;
-                  } else if (name === "fuelLevel") {
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   fuelLevel: characteristic,
-                    // }));
-                    // newChars.fuelLevel = characteristic;
-                  } else if (name === "absoluteLoad") {
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   absoluteLoad: characteristic,
-                    // }));
-                    // newChars.absoluteLoad = characteristic;
-                  } else if (name === "relativeThrottlePosition") {
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   relativeThrottlePosition: characteristic,
-                    // }));
-                    // newChars.relativeThrottlePosition = characteristic;
-                  } else if (name === "engineFuelRate") {
-                    // setChars((prevChars) => ({
-                    //   ...prevChars,
-                    //   engineFuelRate: characteristic,
-                    // }));
-                    // newChars.engineFuelRate = characteristic;
-                  }
-                  setChars(newChars);
-                });
-                // console.log(characteristic.value);
-                // console.log(characteristic.descriptors);
-              });
-            });
-          });
-        });
-    });
-  }, [allDevices]);
-
-  useEffect(() => {
+    NavigationBar.setVisibilityAsync("hidden");
     setLandscapeLeftOrientation();
-    scanForDevices();
-    let job = { id: -1 };
-    const action = () => {
-      // console.log("action")
-      // fetch("http://localhost:1234/")
-      //   .then((response) => response.json())
-      //   .then((data) => {
-      //     setJson(data);
-      //     job.id = requestAnimationFrame(action);
-      //   })
-      //   .catch((error) => console.log(error));
-      const blePromises = [Promise.resolve()];
+    bleController.requestPermissions();
 
-      if (chars?.engineRPM) {
-        console.log("asking for RPM");
-        blePromises.engineRPM = chars?.engineRPM?.read().then((data) => {
-          return { engineRPM: base64.decode(data.value) };
-        });
-      }
-      if (chars?.vehicleSpeed) {
-        blePromises.vehicleSpeed = chars?.vehicleSpeed?.read().then((data) => {
-          return { vehicleSpeed: base64.decode(data.value) };
-        });
-      }
-      if (chars?.engineLoad) {
-        // chars?.engineLoad?.read().then((data) => {
-        //   // console.log(base64.decode(data.value));
-        //   bleJson.engineLoad = base64.decode(data.value);
-        //   setJson(bleJson);
-        // });
-        blePromises.engineLoad = chars?.engineLoad?.read().then((data) => {
-          return { engineLoad: base64.decode(data.value) };
-        });
-      }
-      if (chars?.engineFuelRate) {
-        // chars?.engineFuelRate?.read().then((data) => {
-        //   // console.log(base64.decode(data.value));
-        //   bleJson.engineFuelRate = base64.decode(data.value);
-        //   setJson(bleJson);
-        // });
-        blePromises.engineFuelRate = chars?.engineFuelRate?.read().then((data) => {
-          return { engineFuelRate: base64.decode(data.value) };
-        });
-      }
-      if (chars?.uptime) {
-        // chars?.uptime?.read().then((data) => {
-        //   // console.log(base64.decode(data.value));
-        //   bleJson.uptime = base64.decode(data.value);
-        //   setJson(bleJson);
-        // });
-        blePromises.uptime = chars?.uptime?.read().then((data) => {
-          return { uptime: base64.decode(data.value) };
-        });
-      }
-      if (chars?.fuelLevel) {
-        // chars?.fuelLevel?.read().then((data) => {
-        //   // console.log(base64.decode(data.value));
-        //   bleJson.fuelLevel = base64.decode(data.value);
-        //   setJson(bleJson);
-        // });
-        blePromises.fuelLevel = chars?.fuelLevel?.read().then((data) => {
-          return { fuelLevel: base64.decode(data.value) };
-        });
-      }
-      if (chars?.absoluteLoad) {
-        // chars?.absoluteLoad?.read().then((data) => {
-        //   // console.log(base64.decode(data.value));
-        //   bleJson.absoluteLoad = base64.decode(data.value);
-        //   setJson(bleJson);
-        // });
-        blePromises.absoluteLoad = chars?.absoluteLoad?.read().then((data) => {
-          return { absoluteLoad: base64.decode(data.value) };
-        });
-      }
-      if (chars?.relativeThrottlePosition) {
-        // chars?.relativeThrottlePosition?.read().then((data) => {
-        //   // console.log(base64.decode(data.value));
-        //   bleJson.relativeThrottlePosition = base64.decode(data.value);
-        //   setJson(bleJson);
-        // });
-        blePromises.relativeThrottlePosition = chars?.relativeThrottlePosition?.read().then((data) => {
-          return { relativeThrottlePosition: base64.decode(data.value) };
-        });
-      }
-      // setJson(bleJson);
-      let bleJson = {};
-      // console.log("looks promising");
-      Promise.all(blePromises).then((data) => {
-          bleJson = {...bleJson, ...data};
-          // console.log('lets set');
-          setJson(bleJson);''
-          job.id = requestAnimationFrame(action);
-        }).catch((error) => console.log(error));
-
+    return () => {
+      bleController.disconnect();
     };
-    action();
-     return () => cancelAnimationFrame(job.id);
-  }, [chars]);
-
-  const setLandscapeLeftOrientation = async () => {
-    await ScreenOrientation.lockAsync(
-      ScreenOrientation.OrientationLock.LANDSCAPE_LEFT
-    );
-    setLandscape(true);
-  };
+  }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
         <View style={styles.valuesColumn}>
           <Text style={[styles.styledText, styles.speedValueText]}>
-            {json?.vehicleSpeed}
+            {dataFrame?.vehicleSpeed}
           </Text>
         </View>
         <View style={styles.unitsColumn}>
@@ -265,15 +33,31 @@ export default function HomeScreen() {
         </View>
         <View style={styles.valuesColumn}>
           <Text style={[styles.styledText, styles.rpmValueText]}>
-            {json?.engineRPM}
+            {dataFrame?.engineRPM}
           </Text>
         </View>
         <View style={styles.unitsColumn}>
           <Text style={[styles.styledText, styles.unitsText]}>rpm</Text>
+          <Button
+            title="Connect"
+            onPress={() => bleController.connect(setDataFrame)}
+          />
+          <Text style={styles.connectionIndicator}>
+            {bleController.connectedDevice
+              ? "Device connected"
+              : "No device connected"}
+          </Text>
         </View>
       </View>
     </View>
   );
+
+  async function setLandscapeLeftOrientation() {
+    await ScreenOrientation.lockAsync(
+      ScreenOrientation.OrientationLock.LANDSCAPE_LEFT
+    );
+    setLandscape(true);
+  }
 }
 
 const styles = StyleSheet.create({
@@ -313,5 +97,9 @@ const styles = StyleSheet.create({
     paddingBottom: 87,
     paddingLeft: 10,
     color: "#c5c8c8",
+  },
+  connectionIndicator: {
+    color: "white",
+    fontSize: 20,
   },
 });
---
+++ DashingApp/hooks/useBleController.ts
@@ -0,0 +1,191 @@
+import { useRef, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+import { platformApiLevel } from "expo-device";
+import base64 from "react-native-base64";
+import { BleManager, Device } from "react-native-ble-plx";
+
+export interface DataFrame {
+  engineLoad: number;
+  engineRPM: number;
+  vehicleSpeed: number;
+  uptime: number;
+  fuelLevel: number;
+  absoluteLoad: number;
+  relativeThrottlePosition: number;
+  engineFuelRate: number;
+}
+
+const DATA_SERVICE_UUID = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const COLOR_CHARACTERISTIC_UUID = "19b10001-e8f2-537e-4f6c-d104768a1217";
+
+const bleManager = new BleManager();
+
+export function useBleController() {
+  const discoveredDevices = useRef<Device[]>([]);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+
+  scanForDevices();
+
+  return {
+    connect,
+    disconnect,
+    connectedDevice,
+    requestPermissions,
+  };
+
+  async function connect(onUpdate: (data: DataFrame) => void) {
+    if (connectedDevice && (await connectedDevice.isConnected())) {
+      console.log("Device already connected");
+      return true;
+    }
+
+    await bleManager.stopDeviceScan();
+    while (discoveredDevices.current.length > 0) {
+      const device = discoveredDevices.current.pop()!;
+
+      let connectedDevice;
+      try {
+        connectedDevice = await device.connect({ requestMTU: 512 });
+        await connectedDevice.discoverAllServicesAndCharacteristics();
+      } catch (e) {
+        console.log("FAILED TO CONNECT", e);
+        continue;
+      }
+
+      const controllerCharacteristic = await getControllerCharacteristic(
+        connectedDevice
+      );
+      if (!controllerCharacteristic) {
+        continue;
+      }
+
+      controllerCharacteristic.monitor((data, char) => {
+        console.log(base64.decode(char!.value!));
+        onUpdate(JSON.parse(base64.decode(char!.value!)));
+      });
+
+      setConnectedDevice(connectedDevice);
+      return true;
+    }
+
+    console.log("No device to connect to. Try again later.");
+    scanForDevices();
+    return false;
+  }
+
+  async function disconnect() {
+    if (!connectedDevice) {
+      return;
+    }
+
+    await connectedDevice.cancelConnection();
+    setConnectedDevice(null);
+    scanForDevices();
+  }
+
+  async function scanForDevices() {
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      const targetDeviceName = "DashingBoard";
+      if (error) {
+        console.log(error);
+      } else if (
+        device &&
+        (device.localName === targetDeviceName ||
+          device.name === targetDeviceName)
+      ) {
+        if (isDuplicateDevice(discoveredDevices.current, device)) {
+          return;
+        }
+        discoveredDevices.current.push(device);
+      }
+    });
+  }
+}
+
+async function getControllerCharacteristic(device: Device) {
+  try {
+    const services = await device.services();
+    for (const service of services) {
+      const characteristics = await service.characteristics();
+      for (const characteristic of characteristics) {
+        const labelUUID = "2901";
+
+        const descriptors = await characteristic.descriptors();
+        const labelDescriptor = descriptors.find(
+          (descriptor) => descriptor.uuid === labelUUID
+        );
+
+        if (!labelDescriptor || !labelDescriptor.value) {
+          continue;
+        }
+
+        const name = base64.decode(labelDescriptor.value);
+        if (name === "json") {
+          return characteristic;
+        }
+      }
+    }
+  } catch (e) {
+    console.log("FAILED TO FIND CONTROLLER SERVICE", e);
+  }
+
+  return null;
+}
+
+async function requestPermissions() {
+  if (Platform.OS === "android") {
+    if ((platformApiLevel ?? -1) < 31) {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Location Permission",
+          message: "Bluetooth Low Energy requires Location",
+          buttonPositive: "OK",
+        }
+      );
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    } else {
+      const isAndroid31PermissionsGranted = await requestAndroid31Permissions();
+      return isAndroid31PermissionsGranted;
+    }
+  } else {
+    return true;
+  }
+}
+
+async function requestAndroid31Permissions() {
+  const bluetoothScanPermission = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+    {
+      title: "Location Permission",
+      message: "Bluetooth Low Energy requires Location",
+      buttonPositive: "OK",
+    }
+  );
+  const bluetoothConnectPermission = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+    {
+      title: "Location Permission",
+      message: "Bluetooth Low Energy requires Location",
+      buttonPositive: "OK",
+    }
+  );
+  const fineLocationPermission = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    {
+      title: "Location Permission",
+      message: "Bluetooth Low Energy requires Location",
+      buttonPositive: "OK",
+    }
+  );
+
+  return (
+    bluetoothScanPermission === "granted" &&
+    bluetoothConnectPermission === "granted" &&
+    fineLocationPermission === "granted"
+  );
+}
+
+function isDuplicateDevice(devices: Device[], nextDevice: Device) {
+  return devices.findIndex((device) => nextDevice.id === device.id) > -1;
+}
